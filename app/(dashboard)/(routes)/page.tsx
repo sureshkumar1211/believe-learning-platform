fe9546ca755f92af65525590a2f60d38fdeace6b
Sureshkumar Karuppusamy <sureshkumarkdsk@gmail.com>
2023-09-23 22:41:03 +0530

    Authentication completed with clerk provider

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <h1 className="text-2xl font-medium text-gray-900">
         Welcome to believe learning platform!
       </h1>
-      <Button variant={"default"}>Click</Button>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
